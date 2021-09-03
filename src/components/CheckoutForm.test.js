06a36bec8f54cf3b44577ed39501421bb747e9af
Richard Oh <n24512@protonmail.com>
2021-09-03 11:35:19 -0500

    completed sprint

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -10,15 +10,33 @@ test("renders without errors", () => {
   render(<CheckoutForm />);
 });
 
-test("shows success message on submit with form details", () => {
+test("shows success message on submit with form details", async () => {
   render(<CheckoutForm />);
 
   const firstNameInput = screen.getByLabelText(/first name:/i);
   userEvent.type(firstNameInput, "Richard");
+  const lastNameInput = screen.getByLabelText(/last name:/i);
+  userEvent.type(lastNameInput, "Oh");
+  const addressInput = screen.getByLabelText(/address:/i);
+  userEvent.type(addressInput, "123 Some Street");
+  const cityInput = screen.getByLabelText(/city:/i);
+  userEvent.type(cityInput, "Honolulu");
+  const stateInput = screen.getByLabelText(/state:/i);
+  userEvent.type(stateInput, "Hawaii");
+  const zipInput = screen.getByLabelText(/zip:/i);
+  userEvent.type(zipInput, "00000");
 
   const submit = screen.getByRole("button");
   userEvent.click(submit);
 
-  const successMessage = screen.queryByTestId("successMessage");
-  expect(successMessage).toBeInTheDocument();
+  await waitFor(() => {
+    const successMessage = screen.queryByTestId("successMessage");
+    expect(successMessage).toBeInTheDocument();
+    expect(successMessage).toHaveTextContent("Richard");
+    expect(successMessage).toHaveTextContent("Oh");
+    expect(successMessage).toHaveTextContent("123 Some Street");
+    expect(successMessage).toHaveTextContent("Honolulu");
+    expect(successMessage).toHaveTextContent("Hawaii");
+    expect(successMessage).toHaveTextContent("00000");
+  });
 });
