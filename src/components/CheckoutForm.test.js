a362969829ed8e650359e6baea75c6a9d3d51322
Richard Oh <n24512@protonmail.com>
2021-09-03 10:51:20 -0500

    added required dependencies to CheckoutForm.test.js

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MutationObserver from "mutationobserver-shim";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -9,16 +10,15 @@ test("renders without errors", () => {
   render(<CheckoutForm />);
 });
 
-test("shows success message on submit with form details", async () => {
+test("shows success message on submit with form details", () => {
   render(<CheckoutForm />);
 
-  const firstNameInput = screen.getByDisplayValue(values.firstName);
-  console.log(firstNameInput);
-
+  const firstNameInput = screen.getByLabelText(/first name:/i);
   userEvent.type(firstNameInput, "Richard");
 
-  await waitFor(() => {
-    const firstName = screen.queryByTestId("successMessage");
-    expect(firstName).toBeInTheDocument();
-  });
+  const submit = screen.getByRole("button");
+  userEvent.click(submit);
+
+  const successMessage = screen.queryByTestId("successMessage");
+  expect(successMessage).toBeInTheDocument();
 });
